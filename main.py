ea84c851b89f2ff5c570d9cd24af3c960d43c9c9
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-06-04 18:12:15 +0300

    Minor changes

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from datetime import date
 from application.salary import calculate_salary
 from application.db.people import get_employees
 
+
 def nowdate():
     return date.today()
+
 
 if __name__ == '__main__':
     calculate_salary(nowdate())
